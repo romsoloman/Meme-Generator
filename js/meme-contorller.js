08e93c18556d01ef86e2934be3de18c9da6ffc94
Rom Soloman <romsoloman19@gmail.com>
2021-02-10 13:16:29 +0200

    render gallery

--- js/meme-contorller.js
+++ js/meme-contorller.js
@@ -1,13 +1,33 @@
 'use strict'
 
 
+const init = () => {
+    renderGallery();
+}
+
 const onImgClicked = elImg => {
     openEditor();
 }
 
 const openEditor = () => {
-    const elGallery = document.querySelector('.main-content');
+    const elMainContent = document.querySelector('.main-content');
     const elEditor = document.querySelector('.meme-editor-container');
-    elGallery.style.display = 'none';
+    elMainContent.style.display = 'none';
     elEditor.style.display = 'grid';
+}
+
+const openGallery = () => {
+    const elMainContent = document.querySelector('.main-content');
+    const elEditor = document.querySelector('.meme-editor-container');
+    elEditor.style.display = 'none';
+    elMainContent.style.display = 'grid';
+}
+
+const renderGallery = () => {
+    const imgs = getImgs();
+    const strHTMLs = imgs.map(img => {
+        return `<img src="img/${img.id}.jpg" onclick="onImgClicked(this)">`
+    }).join(' ');
+    const elGallery = document.querySelector('.gallery-container');
+    elGallery.innerHTML = strHTMLs;
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,28 @@
 'use strict'
+
+
+const gImgs = [
+    { id: 1, url: 'img/1.jpg', keywords: ['trump'] },
+    { id: 2, url: 'img/2.jpg', keywords: ['trump'] },
+    { id: 3, url: 'img/3.jpg', keywords: ['trump'] },
+    { id: 4, url: 'img/4.jpg', keywords: ['trump'] },
+    { id: 5, url: 'img/5.jpg', keywords: ['trump'] },
+    { id: 6, url: 'img/6.jpg', keywords: ['trump'] },
+    { id: 7, url: 'img/7.jpg', keywords: ['trump'] },
+    { id: 8, url: 'img/8.jpg', keywords: ['trump'] },
+    { id: 9, url: 'img/9.jpg', keywords: ['trump'] },
+    { id: 10, url: 'img/10.jpg', keywords: ['trump'] },
+    { id: 11, url: 'img/11.jpg', keywords: ['trump'] },
+    { id: 12, url: 'img/12.jpg', keywords: ['trump'] },
+    { id: 13, url: 'img/13.jpg', keywords: ['trump'] },
+    { id: 14, url: 'img/14.jpg', keywords: ['trump'] },
+    { id: 15, url: 'img/15.jpg', keywords: ['trump'] },
+    { id: 16, url: 'img/16.jpg', keywords: ['trump'] },
+    { id: 17, url: 'img/17.jpg', keywords: ['trump'] },
+    { id: 18, url: 'img/18.jpg', keywords: ['trump'] },
+];
+
+
+const getImgs = () => {
+    return gImgs;
+}
