842a9e0d8f745836443c2d82589189e9af19318a
Rom Soloman <romsoloman19@gmail.com>
2021-02-10 12:57:26 +0200

    add meme editor onclick img

--- js/meme-contorller.js
+++ js/meme-contorller.js
@@ -1,1 +1,13 @@
 'use strict'
+
+
+const onImgClicked = elImg => {
+    openEditor();
+}
+
+const openEditor = () => {
+    const elGallery = document.querySelector('.main-content');
+    const elEditor = document.querySelector('.meme-editor-container');
+    elGallery.style.display = 'none';
+    elEditor.style.display = 'grid';
+}
